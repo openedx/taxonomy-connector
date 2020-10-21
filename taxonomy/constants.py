478b1f4087d329d96d18893b83f4b033d50b70df
Zaman Afzal <zamanafzal@gmail.com>
2020-10-21 10:54:03 +0500

    ENT-3533 Get the jobs according to date (#22)

--- taxonomy/constants.py
+++ taxonomy/constants.py
@@ -3,26 +3,23 @@
 Constants used by the taxonomy connector.
 """
 
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
+
 JOBS_QUERY_FILTER = {
     'filter': {
         'when': {
-            'start': '2020-01',
-            'end': '2020-06',
+            'start': str(date.today() - relativedelta(months=6)),
+            'end': str(date.today()),
             'type': 'active'
         },
-        'posting_duration': {
-            'lower_bound': 0,
-            'upper_bound': 90
-        }
     },
     'rank': {
         'by': 'unique_postings',
-        'min_unique_postings': 1000,
-        'limit': 20
     },
     'nested_rank': {
         'by': 'significance',
-        'limit': 10
     }
 }
 
