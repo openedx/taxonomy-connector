725ea6d2ed6f6a41a48bc15c741b766284bec1f4
zamanazal <zamanafzal@gmail.com>
2020-11-04 11:26:15 +0500

    ENT-3534 Get the jobs-salary for last 6 months

--- taxonomy/constants.py
+++ taxonomy/constants.py
@@ -26,19 +26,13 @@ JOBS_QUERY_FILTER = {
 JOB_POSTINGS_QUERY_FILTER = {
     'filter': {
         'when': {
-            'start': '2020-01',
-            'end': '2020-06',
+            'start': str(date.today() - relativedelta(months=6)),
+            'end': str(date.today()),
             'type': 'active'
-        },
-        'posting_duration': {
-            'lower_bound': 0,
-            'upper_bound': 90
         }
     },
     'rank': {
         'by': 'unique_postings',
-        'min_unique_postings': 1000,
-        'limit': 20,
         'extra_metrics': [
             'median_posting_duration',
             'median_salary',
