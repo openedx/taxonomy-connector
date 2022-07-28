c087cd6438377bf57a10586ea6dda7b9714c3800
AliAkbar <aliakbarit019@gmail.com>
2022-07-28 13:17:20 +0500

    feat: use program update signal to call EMSI API

--- taxonomy/signals/handlers.py
+++ taxonomy/signals/handlers.py
@@ -8,7 +8,7 @@ from django.dispatch import receiver
 
 from taxonomy.tasks import update_course_skills
 
-from .signals import UPDATE_COURSE_SKILLS
+from .signals import UPDATE_COURSE_SKILLS, UPDATE_PROGRAM_SKILLS
 
 LOGGER = logging.getLogger(__name__)
 
@@ -20,3 +20,11 @@ def handle_update_course_skills(sender, course_uuid, **kwargs):  # pylint: disab
     """
     LOGGER.info('[TAXONOMY] UPDATE_COURSE_SKILLS signal received')
     update_course_skills.delay([course_uuid])
+
+
+@receiver(UPDATE_PROGRAM_SKILLS)
+def handle_update_program_skills(sender, program_uuid, **kwargs):  # pylint: disable=unused-argument
+    """
+    Handle signal and trigger task to update program skills.
+    """
+    # TODO: to be added in https://2u-internal.atlassian.net/browse/PROD-2904
