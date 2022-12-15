7742ea92d7b4cdcea863073394fca661d52cc490
Pooja Kulkarni <pooja@opencraft.com>
2022-12-15 14:16:10 -0500

    fix: Additional test cases to complete coverage

--- tests/management/test_finalize_xblockskill_tags.py
+++ tests/management/test_finalize_xblockskill_tags.py
@@ -12,6 +12,7 @@ from test_utils import factories
 from test_utils.testcase import TaxonomyTestCase
 from test_utils.constants import USAGE_KEY
 from taxonomy.models import XBlockSkillData
+from taxonomy.exceptions import InvalidCommandOptionsError
 
 
 @mark.django_db
@@ -34,7 +35,7 @@ class FinalizeSkillTagsCommandTests(TaxonomyTestCase):
         unverified_skills = XBlockSkillData.objects.filter(verified=False)
         self.assertEqual(len(unverified_skills), 0)
         with LogCapture(level=logging.INFO) as log_capture:
-            call_command(self.command)
+            call_command(self.command, min_votes=2, ratio_threshold=0.5)
             self.assertEqual(len(log_capture.records), 2)
             messages = [record.msg for record in log_capture.records]
             self.assertEqual(
@@ -49,8 +50,9 @@ class FinalizeSkillTagsCommandTests(TaxonomyTestCase):
     def test_finalize_xblockskill_tags_below_minimum_votes(self):
         """
         Test that command only shows starting and completed logs
-        if verified count is below MIN_VOTES_FOR_SKILLS.
-        The MIN_VOTES_FOR_SKILLS value is set to 2 in test_settings
+        if verified count is below minimum votes.
+        The minimum votes can either be configured in settings file
+        using MIN_VOTES_FOR_SKILLS or passed in as an optional parameter.
         """
         xblock = factories.XBlockSkillsFactory(usage_key=USAGE_KEY)
         xblock_skill = factories.XBlockSkillDataFactory(xblock=xblock)
@@ -64,7 +66,7 @@ class FinalizeSkillTagsCommandTests(TaxonomyTestCase):
         xblock_skill.ignored_count = 0
         xblock_skill.save()
         with LogCapture(level=logging.INFO) as log_capture:
-            call_command(self.command)
+            call_command(self.command, min_votes=2, ratio_threshold=0.5)
             self.assertEqual(len(log_capture.records), 2)
             messages = [record.msg for record in log_capture.records]
             self.assertEqual(
@@ -80,8 +82,10 @@ class FinalizeSkillTagsCommandTests(TaxonomyTestCase):
         """
         Test that command only shows starting and completed logs
         if the ratio of verified_count to ignored_count is below
-        the RATIO_THRESHOLD_FOR_SKILLS.
-        The RATIO_THRESHOLD_FOR_SKILLS value is set to 0.5 in test_settings
+        the ratio threshold.
+        The ratio threshold can either be configured in the settings
+        file using RATIO_THRESHOLD_FOR_SKILLS or passed in as an
+        optional parameter.
         """
         xblock = factories.XBlockSkillsFactory(usage_key=USAGE_KEY)
         xblock_skill = factories.XBlockSkillDataFactory(xblock=xblock)
@@ -96,7 +100,7 @@ class FinalizeSkillTagsCommandTests(TaxonomyTestCase):
         xblock_skill.ignored_count = 3
         xblock_skill.save()
         with LogCapture(level=logging.INFO) as log_capture:
-            call_command(self.command)
+            call_command(self.command, min_votes=2, ratio_threshold=0.5)
             self.assertEqual(len(log_capture.records), 2)
             messages = [record.msg for record in log_capture.records]
             self.assertEqual(
@@ -126,7 +130,7 @@ class FinalizeSkillTagsCommandTests(TaxonomyTestCase):
         xblock_skill.ignored_count = 1
         xblock_skill.save()
         with LogCapture(level=logging.INFO) as log_capture:
-            call_command(self.command)
+            call_command(self.command, min_votes=2, ratio_threshold=0.5)
             self.assertEqual(len(log_capture.records), 3)
             messages = [record.msg for record in log_capture.records]
             self.assertEqual(
@@ -139,3 +143,92 @@ class FinalizeSkillTagsCommandTests(TaxonomyTestCase):
             )
         updated_xblockskill = XBlockSkillData.objects.all()[0]  # there's only one
         self.assertTrue(updated_xblockskill.verified)
+
+    @responses.activate
+    def test_finalize_xblockskill_tags_without_min_votes(self):
+        """
+        Test that command raises InvalidCommandOption error if
+        minimum votes is not set in the settings file or sent in
+        as a parameter
+        """
+        xblock = factories.XBlockSkillsFactory(usage_key=USAGE_KEY)
+        xblock_skill = factories.XBlockSkillDataFactory(xblock=xblock)
+
+        # ensure xblockskilldata object is created
+        unverified_skills = XBlockSkillData.objects.filter(verified=False)
+        self.assertEqual(len(unverified_skills), 1)
+
+        xblock_skill.verified_count = 1
+        xblock_skill.ignored_count = 0
+        xblock_skill.save()
+
+        error_str = 'Either configure MIN_VOTES_FOR_SKILLS in settings or pass with arg --min-votes'
+        with LogCapture(level=logging.INFO) as log_capture:
+            with self.assertRaisesRegex(InvalidCommandOptionsError, error_str):
+                call_command(self.command, ratio_threshold=0.5)
+            self.assertEqual(len(log_capture.records), 1)
+            message = log_capture.records[0].msg
+            # only the staring message will be shown
+            self.assertEqual(
+                message,
+                'Starting xblockskill tags verification task',
+            )
+
+    @responses.activate
+    def test_finalize_xblockskill_tags_without_ratio_threshold(self):
+        """
+        Test that command raises InvalidCommandOption error if the
+        ratio threshold is not set in the settings file or sent in
+        as a parameter
+        """
+        xblock = factories.XBlockSkillsFactory(usage_key=USAGE_KEY)
+        xblock_skill = factories.XBlockSkillDataFactory(xblock=xblock)
+
+        # ensure xblockskilldata object is created
+        unverified_skills = XBlockSkillData.objects.filter(verified=False)
+        self.assertEqual(len(unverified_skills), 1)
+
+        xblock_skill.verified_count = 1
+        xblock_skill.ignored_count = 0
+        xblock_skill.save()
+
+        error_str = 'Either configure RATIO_THRESHOLD_FOR_SKILLS in settings or pass with arg --ratio-threshold'
+        with LogCapture(level=logging.INFO) as log_capture:
+            with self.assertRaisesRegex(InvalidCommandOptionsError, error_str):
+                call_command(self.command, min_votes=2)
+            self.assertEqual(len(log_capture.records), 1)
+            message = log_capture.records[0].msg
+            # only the staring message will be shown
+            self.assertEqual(
+                message,
+                'Starting xblockskill tags verification task',
+            )
+
+    @responses.activate
+    def test_finalize_xblockskill_tags_without_any_counts(self):
+        """
+        Test that command only shows starting and completed logs
+        if the skills have neither verified not ignored counts
+        """
+        xblock = factories.XBlockSkillsFactory(usage_key=USAGE_KEY)
+        xblock_skill = factories.XBlockSkillDataFactory(xblock=xblock)
+
+        # ensure xblockskilldata object is created
+        unverified_skills = XBlockSkillData.objects.filter(verified=False)
+        self.assertEqual(len(unverified_skills), 1)
+
+        xblock_skill.verified_count = 0
+        xblock_skill.ignored_count = 0
+        xblock_skill.save()
+
+        with LogCapture(level=logging.INFO) as log_capture:
+            call_command(self.command, min_votes=2, ratio_threshold=0.5)
+            self.assertEqual(len(log_capture.records), 2)
+            messages = [record.msg for record in log_capture.records]
+            self.assertEqual(
+                messages,
+                [
+                    'Starting xblockskill tags verification task',
+                    'Xblockskill tags verification task is completed'
+                ]
+            )
